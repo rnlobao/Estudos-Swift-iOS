2104b85408cc6f2186f46e6e9d6d8e8d9fc71494
Robson Novato <rnlobao42@gmail.com>
2022-07-13 18:30:02 -0300

    keep learning closures

--- closures.playground/Contents.swift
+++ closures.playground/Contents.swift
@@ -8,3 +8,21 @@
  */
 
 //all functions are closures but not all closures are functions
+/*
+ there are three forms of closures:
+ 1) GLobal functions: closures with name and dont capture values
+ 2) nested functions: closures with name and capture values
+ 3) closure expressions: unnamed and capture values
+*/
+//Closures sometimes make functions easier to use and more adaptable
+//the in word mark the end of the parameters and the beginning of the code
+
+
+
+
+//We can use _ to make a parameter of a function not mandatory so we can pass only the value
+func printarNome(_ nome: String) -> String {
+    return nome
+}
+print(printarNome("aaa"))
+

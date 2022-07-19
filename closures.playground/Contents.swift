4e42ea64986b7c902cf268bae67b0aba11400741
Robson Novato <rnlobao42@gmail.com>
2022-07-19 10:51:47 -0300

    closures

--- closures.playground/Contents.swift
+++ closures.playground/Contents.swift
@@ -17,8 +17,40 @@
 //Closures sometimes make functions easier to use and more adaptable
 //the in word mark the end of the parameters and the beginning of the code
 
+//lets transform a function into a closure
+func justAdd(num1: Int, num2: Int) -> Int {
+    return num1 + num2
+}
+print(justAdd(num1: 1, num2: 2))
 
+let add = { (num1: Int, num2: Int) -> Int in
+    return num1 + num2
+}
+print(add(1, 3))
 
+//Another way to use the add function as a closure is passing the type of the variable and than naming it
+let add1: (Int, Int) -> Int = { (num3, num4) in
+    return num3 + num4
+}
+print(add1(20, 20))
+
+let add2: (Int, Int) -> Int = {
+    return $0 + $1
+}
+print(add2(3, 12))
+
+//We can also create an array of functions, look
+let multiplyy = { (num1: Int, num2: Int) -> Int in
+    return num1 * num2
+}
+
+let dividee = { (num1: Int, num2: Int) -> Int in
+    return num1 / num2
+}
+
+let arrOfFunc = [multiplyy, dividee]
+let letsMultiply = arrOfFunc[0]
+print(letsMultiply(2, 3))
 
 //We can use _ to make a parameter of a function not mandatory so we can pass only the value
 func printarNome(_ nome: String) -> String {

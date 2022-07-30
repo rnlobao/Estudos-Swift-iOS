8943d1cb06de6c29fe22ce95107f0bd7c0cd4c11
Robson Novato <rnlobao42@gmail.com>
2022-07-30 14:23:46 -0300

    initialization basic

--- initialization.playground/Contents.swift
+++ initialization.playground/Contents.swift
@@ -1,3 +1,26 @@
-import UIKit
+//inicializacao
+//Voce deve passar sempre pra uma classe quando os valores estocados nao tem um valor padrao ou sao opcionais
 
-var greeting = "Hello, playground"
+class Carro {
+    var rodas: Int
+    var cor: String
+    var quatroPortas: Bool
+    
+    init(rodas: Int, cor: String, quatroPortas: Bool) {
+        self.rodas = rodas
+        self.cor = cor
+        self.quatroPortas = quatroPortas
+    }
+}
+
+var meuCarro = Carro(rodas: 4, cor: "Azul", quatroPortas: true)
+
+class Moto {
+    var cor: String = "Verde"
+    var xenon: Bool = false
+    var cilindradas: Int = 200
+}
+
+var minhaMoto = Moto()
+
+

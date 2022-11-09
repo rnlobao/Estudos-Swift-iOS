c5552e0f3fad464c8558dc75c3622713a3e5520f
ROBSON NOVATO LOBAO <rnlobao42@gmail.com>
2022-11-09 11:08:10 -0300

    concurrency

--- concurrency.playground/Contents.swift
+++ concurrency.playground/Contents.swift
@@ -1,3 +1,28 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//podemos usar funcoes assincronas para esperar o resultado de uma operacao dentro de uma funcao e nao cair em estado de erro por exemplo
+
+func demorada() -> String {
+    return "algo"
+}
+
+func esperarAlgo() async -> String {
+    let result: String = await demorada()
+    return result
+}
+
+//USamos o await para saber qual funcao deve ser esperada por um resultado
+
+//para rodar algo em paralelo é só colocar o async na frente de um let
+
+
+func parallelFunc() async {
+    async let primeiro = demorada()
+    async let segundo = demorada()
+    async let terceiro = demorada()
+    
+    _ = await [primeiro, segundo, terceiro]
+}
+
+
+

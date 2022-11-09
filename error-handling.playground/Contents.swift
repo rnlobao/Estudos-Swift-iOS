6237ba529c35fc836ba5293ba4f375b565e45f6d
ROBSON NOVATO LOBAO <rnlobao42@gmail.com>
2022-11-08 21:18:22 -0300

    lidando com erros

--- error-handling.playground/Contents.swift
+++ error-handling.playground/Contents.swift
@@ -1,2 +1,37 @@
 import UIKit
 
+// podemos ter varios tipos de erro que sao dados com throw e representados pelo tipo Error
+
+enum pcErrors: Error {
+    case memoriaInsuficiente
+    case semBateria
+    case ramPifou
+}
+
+throw pcErrors.memoriaInsuficiente
+
+// Há 4 formas de lidar com um erro em swift:
+/*
+ 1) Propagar o erro
+ 2) lidar usando um do - catch
+ 3) lidar sendo um valor optional
+ 4) nao ter erros rs
+ */
+
+//Uma funcao pode dar throw ou nao em um erro:
+func canThrowError() throws -> String {
+    throw pcErrors.memoriaInsuficiente
+}
+var memoriaAtual = 10
+func tentarAlocarMemoria(valor: Int) -> Int {
+    return memoriaAtual - valor
+}
+
+// nos usamos o try para tentar algo, se retornar erro no do, entra no catch
+
+do {
+    try canThrowError()
+    print("Sucesso")
+} catch pcErrors.memoriaInsuficiente {
+    print("Memoria Insuficiente")
+}

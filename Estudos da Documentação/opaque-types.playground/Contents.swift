f41291cda4e186a86d810c72dc358f7a13f57032
ROBSON NOVATO LOBAO <rnlobao42@gmail.com>
2023-01-03 22:28:31 -0300

    opaque types

--- Estudos da Documentação/opaque-types.playground/Contents.swift
+++ Estudos da Documentação/opaque-types.playground/Contents.swift
@@ -1,3 +1,18 @@
-import UIKit
+// Opaque Types
 
-var greeting = "Hello, playground"
+// basicamente utilizamos o some de retorno pra poder fazer o retorno de qualquer tipo de dado
+
+protocol Fighter { }
+struct XWing: Fighter { }
+
+func launchFighter() -> Fighter {
+    XWing()
+}
+
+let red5 = launchFighter()
+
+func launchOpaqueFighter() -> some Fighter {
+    XWing()
+}
+
+let red6 = launchOpaqueFighter()

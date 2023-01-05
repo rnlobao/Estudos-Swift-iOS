d9f26a0224c1617f42e3270a281456b72f71e6ec
ROBSON NOVATO LOBAO <66230142+rnlobao@users.noreply.github.com>
2023-01-05 10:28:21 -0300

    acessos

---
+++ Estudos da Documentação/access-control.playground/Contents.swift
@@ -0,0 +1,12 @@
+// Access control
+
+// tipos possiveis de classes:
+
+/*
+ 1 - public: acessa de qualquer lugar e qualquer módulo que esteja importanto
+ 2 - internal: acessa de qualquer lugar do mesmo modulo
+ 3 - fileprivate: funciona so no proprio arquivo
+ 4 - private: fechadao
+ */
+
+

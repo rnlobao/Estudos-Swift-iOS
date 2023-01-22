d9850e45ed10a5a0e524035038408dba87b07a56
ROBSON NOVATO LOBAO <rnlobao42@gmail.com>
2023-01-22 14:53:08 -0300

    inheritance

--- Orientação a Objetos/Object-Oriented-Programming.playground/Contents.swift
+++ Orientação a Objetos/Object-Oriented-Programming.playground/Contents.swift
@@ -1,3 +1,15 @@
+class Music {
+  let notes: [String]
+
+  init(notes: [String]) {
+    self.notes = notes
+  }
+
+  func prepared() -> String {
+    return notes.joined(separator: " ")
+  }
+}
+
 class Instrument {
     let brand: String
     
@@ -8,4 +20,35 @@ class Instrument {
     func tune() -> String {
         fatalError("Implemente!")
     }
+    
+    func play(_ music: Music) -> String {
+      return music.prepared()
+    }
+    
+    func perform(_ music: Music) {
+      print(tune())
+      print(play(music))
+    }
 }
+
+
+class Piano: Instrument {
+  let hasPedals: Bool
+  static let whiteKeys = 52
+  static let blackKeys = 36
+  
+  init(brand: String, hasPedals: Bool = false) {
+    self.hasPedals = hasPedals
+    super.init(brand: brand)
+  }
+  
+  override func tune() -> String {
+    return "Piano standard tuning for \(brand)."
+  }
+  
+  override func play(_ music: Music) -> String {
+    let preparedNotes = super.play(music)
+    return "Piano playing \(preparedNotes)"
+  }
+}
+

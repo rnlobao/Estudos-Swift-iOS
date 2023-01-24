8928aca361bd0375e61770e540ac2ae5d393bbc5
ROBSON NOVATO LOBAO <rnlobao42@gmail.com>
2023-01-23 21:34:46 -0300

    concrete guitars

--- Orientação a Objetos/Object-Oriented-Programming.playground/Contents.swift
+++ Orientação a Objetos/Object-Oriented-Programming.playground/Contents.swift
@@ -52,3 +52,32 @@ class Piano: Instrument {
   }
 }
 
+class Guitar: Instrument {
+  let stringGauge: String
+  
+  init(brand: String, stringGauge: String = "medium") {
+    self.stringGauge = stringGauge
+    super.init(brand: brand)
+  }
+}
+
+class AcousticGuitar: Guitar {
+  static let numberOfStrings = 6
+  static let fretCount = 20
+  
+  override func tune() -> String {
+    return "Tune \(brand) acoustic with E A D G B E"
+  }
+  
+  override func play(_ music: Music) -> String {
+    let preparedNotes = super.play(music)
+    return "Play folk tune on frets \(preparedNotes)."
+  }
+}
+
+let acousticGuitar = AcousticGuitar(brand: "Roland", stringGauge: "light")
+acousticGuitar.tune()
+acousticGuitar.play(Music(notes: ["oi", "aa"]))
+
+
+
